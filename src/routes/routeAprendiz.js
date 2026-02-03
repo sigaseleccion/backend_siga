e38da500644f787bdcf435b6fdcffb471a423031
ppssppf <dickson1611.moreno@gmail.com>
2026-02-02 22:02:54 -0500

    comentado_codigo_token_APR-CONV

--- src/routes/routeAprendiz.js
+++ src/routes/routeAprendiz.js
@@ -4,13 +4,13 @@ const aprendizController = require('../controllers/aprendizController');
 const { verificarToken } = require('../middlewares/authMiddleware');
 
 // Rutas específicas primero (antes de las rutas con parámetros genéricos)
-router.get('/convocatoria/:convocatoriaId', verificarToken, aprendizController.obtenerAprendicesPorConvocatoria);
+router.get('/convocatoria/:convocatoriaId', /*verificarToken*/ aprendizController.obtenerAprendicesPorConvocatoria);
 
 // Rutas generales
-router.get('/', verificarToken, aprendizController.obtenerAprendices);
-router.get('/:id', verificarToken, aprendizController.obtenerAprendizPorId);
-router.post('/', verificarToken, aprendizController.crearAprendiz);
-router.put('/:id', verificarToken, aprendizController.actualizarAprendiz);
-router.delete('/:id', verificarToken, aprendizController.eliminarAprendiz);
+router.get('/', /*verificarToken*/ aprendizController.obtenerAprendices);
+router.get('/:id', /*verificarToken*/ aprendizController.obtenerAprendizPorId);
+router.post('/', /*verificarToken*/ aprendizController.crearAprendiz);
+router.put('/:id', /*verificarToken*/ aprendizController.actualizarAprendiz);
+router.delete('/:id', /*verificarToken*/ aprendizController.eliminarAprendiz);
 
 module.exports = router;
--- src/routes/routeConvocatoria.js
+++ src/routes/routeConvocatoria.js
@@ -4,14 +4,14 @@ const convocatoriaController = require('../controllers/convocatoriaController');
 const { verificarToken } = require('../middlewares/authMiddleware');
 
 // Rutas de convocatorias
-router.get('/', verificarToken, convocatoriaController.obtenerConvocatorias);
-router.get('/:id', verificarToken, convocatoriaController.obtenerConvocatoriaPorId);
-router.post('/', verificarToken, convocatoriaController.crearConvocatoria);
-router.post('/crear-con-aprendices', verificarToken, convocatoriaController.crearConvocatoriaConAprendices);
-router.post('/:id/cargar-excel-adicional', verificarToken, convocatoriaController.cargarExcelAdicional);
-router.put('/:id', verificarToken, convocatoriaController.actualizarConvocatoria);
-router.patch('/:id/cerrar', verificarToken, convocatoriaController.cerrarConvocatoria);
-router.patch('/:id/reabrir', verificarToken, convocatoriaController.reabrirConvocatoria);
-router.patch('/:id/archivar', verificarToken, convocatoriaController.archivarConvocatoria);
+router.get('/', /*verificarToken*/  convocatoriaController.obtenerConvocatorias);
+router.get('/:id', /*verificarToken*/  convocatoriaController.obtenerConvocatoriaPorId);
+router.post('/', /*verificarToken*/  convocatoriaController.crearConvocatoria);
+router.post('/crear-con-aprendices', /*verificarToken*/  convocatoriaController.crearConvocatoriaConAprendices);
+router.post('/:id/cargar-excel-adicional', /*verificarToken*/  convocatoriaController.cargarExcelAdicional);
+router.put('/:id', /*verificarToken*/  convocatoriaController.actualizarConvocatoria);
+router.patch('/:id/cerrar', /*verificarToken*/  convocatoriaController.cerrarConvocatoria);
+router.patch('/:id/reabrir', /*verificarToken*/  convocatoriaController.reabrirConvocatoria);
+router.patch('/:id/archivar', /*verificarToken*/  convocatoriaController.archivarConvocatoria);
 
 module.exports = router;
