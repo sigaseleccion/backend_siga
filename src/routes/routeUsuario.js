88c96f2b45d3d5ac59640de307074d26878f6346
ppssppf <dickson1611.moreno@gmail.com>
2026-02-03 20:25:06 -0500

    comentado verificar token rol-usuarios

--- src/routes/routeUsuario.js
+++ src/routes/routeUsuario.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const usuarioController = require('../controllers/usuarioController');
 const { verificarToken, autorizar } = require('../middlewares/authMiddleware');
 
-router.get('/', verificarToken, autorizar('usuarios', 'ver'), usuarioController.obtenerUsuarios);
-router.get('/:id', verificarToken, autorizar('usuarios', 'ver'), usuarioController.obtenerUsuarioPorId);
-router.post('/', verificarToken, autorizar('usuarios', 'crear'), usuarioController.crearUsuario);
-router.put('/:id', verificarToken, autorizar('usuarios', 'editar'), usuarioController.actualizarUsuario);
-router.delete('/:id', verificarToken, autorizar('usuarios', 'eliminar'), usuarioController.eliminarUsuario);
+router.get('/', /*verificarToken*/ autorizar('usuarios', 'ver'), usuarioController.obtenerUsuarios);
+router.get('/:id', /*verificarToken*/ autorizar('usuarios', 'ver'), usuarioController.obtenerUsuarioPorId);
+router.post('/', /*verificarToken*/ autorizar('usuarios', 'crear'), usuarioController.crearUsuario);
+router.put('/:id', /*verificarToken*/ autorizar('usuarios', 'editar'), usuarioController.actualizarUsuario);
+router.delete('/:id', /*verificarToken*/ autorizar('usuarios', 'eliminar'), usuarioController.eliminarUsuario);
 
 module.exports = router;
