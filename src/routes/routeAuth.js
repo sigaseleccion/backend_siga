85571644102990d4f54a99b438f901ebbbe09691
imzura <zurangely_portillo@soy.sena.edu.co>
2026-02-12 21:55:33 -0500

    actualizacionRecuperarContraseña

--- src/routes/routeAuth.js
+++ src/routes/routeAuth.js
@@ -5,5 +5,7 @@ const { verificarToken } = require('../middlewares/authMiddleware');
 
 router.post('/login', authController.login);
 router.get('/verificar', verificarToken, authController.verificarToken);
-
+router.post("/solicitar-codigo", authController.solicitarCodigoRecuperacion);
+router.post("/verificar-codigo", authController.verificarCodigoRecuperacion);
+router.post("/restablecer-contrasena", authController.restablecerContrasena);
 module.exports = router;
